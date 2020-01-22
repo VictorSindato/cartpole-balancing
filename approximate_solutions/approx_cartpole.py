8a60b6dad0cbccb5d06bd87f84afbdd6621c5f80
Victor P Sindato <vsindato@mac-Barker-5-4.mit.edu>
2020-01-22 14:04:05 -0500

    Adds a method to the Action_Value_Estimator class that performs weight updates using stochastic gradient descent

--- approximate_solutions/approx_cartpole.py
+++ approximate_solutions/approx_cartpole.py
@@ -4,22 +4,44 @@ import random
 import gym
 
 
-class Value_Estimator:
+def get_feature_vector(state, action, order=2):
+    """
+    Inputs:
+        - state: numpy array of state variables
+        - action: numpy array of an action
+        - order: desired highest degree in feature vector
+    """
+
+    # TODO: Be able to calculate feature vectors of various degrees
+
+    return np.append(state, action)
+
+
+class Action_Value_Estimator:
     def __init__(self, num_state_variables):
         self.weights = initialize_weights()
 
-    def initialize_weights(zeros=True):
+    def initialize_weights(self, zeros=True):
         if zeros:
             return np.zeros(num_state_variables)
         return np.random.rand(num_state_variables)
 
-    def get_value(self, state, action):
+    def get_value(self, feature_vector):
+        """
+        Input: [numpy array] feature_vector --> eg. np.array([1,2,3,4])
+        """
+        return np.dot(feature_vector, self.weights)
+
+    # Sarsa
+    def semi_sgd_weight_update(self, current_feature_vector, reward, next_feature_vector, step_size, gamma):
+        """
+        Inputs:
+            - [numpy array] current_feature_vector, next_feature_vector --> eg. np.array([1,2,3,4])
+            - [float] reward, step_size, gamma
+        """
+        target_error = reward + gamma*self.get_value(next_feature_vector) - self.get_value(current_feature_vector)
+        self.weights += step_size * target_error * current_feature_vector   # gradient of value of current feature vector with respect to weights is the feature vector itself
         return
-
-
-
-def semi_sgd_weight_update(state, weights, step_size, target_error):
-    return
 
 
 def e_greedy_action_selection(value_estimator, state, action, epsilon=0.1):
